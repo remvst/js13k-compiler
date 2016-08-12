8a67d483b048d32a5abb1a145f7696c1b3f441e8
remvst <remvst@gmail.com>
2016-08-12 01:28:11 -0400

    Early reloader

--- watch-cmd.js
+++ watch-cmd.js
@@ -2,6 +2,9 @@
 
 const fsp = require('fs-promise');
 const colors = require('colors/safe');
+const express = require('express');
+const http = require('http');
+const reload = require('reload');
 
 const build = require('./build');
 
@@ -11,29 +14,43 @@ fsp.readFile(process.argv.length === 3 ? process.argv[2] : './config.json').catc
 }).then(data => {
     const config = JSON.parse(data.toString());
 
-    return build(config).then(() => {
-        const filesToWatch = [
-            config.INPUT.HTML,
-            config.INPUT.CSS
-        ].concat(config.INPUT.JS);
+    const app = express();
+    app.set('port', 1234);
 
-        function fileModifiedHandler(file){
-            return () => {
-                console.log(file + ' modified\n');
+    const server = http.createServer(app);
 
-                build(config).catch(err => {
-                    console.error(err);
-                }).then(() => {
-                    console.log(colors.underline('\nWaiting for file changes...'));
-                });
-            };
-        }
+    const reloader = reload(server, app);
 
-        console.log(colors.underline('\nWaiting for file changes...'));
 
-        return Promise.all(filesToWatch.map(file => {
-            return fsp.watchFile(file, fileModifiedHandler(file));
-        }));
+    server.listen(app.get('port'), () => {
+        console.log(colors.underline('Reload server is running on port ' + app.get('port') + '\n'));
+
+        build(config).then(() => {
+            const filesToWatch = [
+                config.INPUT.HTML,
+                config.INPUT.CSS
+            ].concat(config.INPUT.JS);
+
+            function fileModifiedHandler(file){
+                return () => {
+                    console.log(file + ' modified\n');
+
+                    build(config).catch(err => {
+                        console.error(err);
+                    }).then(() => {
+                        reloader.reload();
+
+                        console.log(colors.underline('\nWaiting for file changes...'));
+                    });
+                };
+            }
+
+            console.log(colors.underline('\nWaiting for file changes...'));
+
+            return Promise.all(filesToWatch.map(file => {
+                return fsp.watchFile(file, fileModifiedHandler(file));
+            }));
+        });
     });
 }).catch(err => {
     console.error(err);
