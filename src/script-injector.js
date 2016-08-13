deb9f620c372e2d60e2649abef95044933412323
remvst <remvst@gmail.com>
2016-08-12 21:35:32 -0400

    Cleaned up injector

--- src/script-injector.js
+++ src/script-injector.js
@@ -3,15 +3,11 @@
 module.exports = (paths) => {
     let lines = [];
 
-    lines.push('window.addEventListener(\'DOMContentLoaded\', function(){');
-
-    lines.push('    ' + JSON.stringify(paths) + '.forEach(function(path){');
-    lines.push('        var script = document.createElement(\'script\');');
-    lines.push('        script.src = path;');
-    lines.push('        document.body.appendChild(script);');
-    lines.push('    });');
-
-    lines.push('}, false);');
+    lines.push(JSON.stringify(paths) + '.forEach(function(path){');
+    lines.push('    var script = document.createElement(\'script\');');
+    lines.push('    script.src = path;');
+    lines.push('    document.body.appendChild(script);');
+    lines.push('});');
 
     return lines.join('\n');
 };
