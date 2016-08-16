243a17b5c6b56e7a4f9ee85573291540a2aa405e
remvst <remvst@gmail.com>
2016-08-15 22:18:47 -0400

    Fixed backslashes

--- macros/string.js
+++ macros/string.js
@@ -4,7 +4,9 @@ module.exports = {
     'apply': (s) => {
         const str = s.substr(1, s.length - 2);
 
-        return str.split('').map(char => {
+        return str.split('').filter(char => {
+            return char !== '\\';
+        }).map(char => {
             return JSON.stringify(char);
         }).join('+');
     }
