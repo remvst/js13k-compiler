8f414566a89c5300fa8284945ffec8a8ce3dd767
remvst <remvst@gmail.com>
2016-08-31 23:53:43 -0400

    steps > tasks

--- src/compiler.js
+++ src/compiler.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports.run = function(func){
-    const steps = require('./steps'); // get the steps in the global scope
+    const steps = require('./tasks'); // get the steps in the global scope
 
     const rootStep = func(steps);
 
---
+++ src/tasks/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const stepMap = {
+    'loadFiles': require('./load-files'),
+    'parallel': require('./parallel'),
+    'sequence': require('./sequence'),
+    'log': require('./log'),
+    'output': require('./output'),
+    'macros': require('./macros'),
+    'concat': require('./concat'),
+    'mangle': require('./mangle'),
+    'es6ify': require('./es6ify'),
+    'uglifyJS': require('./uglify-js'),
+    'uglifyCSS': require('./uglify-css'),
+    'uglifyHTML': require('./uglify-html'),
+    'pack': require('./pack'),
+    'zip': require('./zip'),
+    'combine': require('./combine'),
+    'constants': require('./constants')
+};
+
+function builderFunction(functionName){
+    const cls = stepMap[functionName];
+    return function(arg){
+        return new cls(arg);
+    };
+}
+
+for(let functionName in stepMap){
+    module.exports[functionName] = builderFunction(functionName);
+}
